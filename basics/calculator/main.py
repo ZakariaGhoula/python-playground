3d5b376fc58c81a5c00549b2f5a2df93b9023b93
Zakaria Ghoula <zghoula@worldia.com>
2026-05-27 12:28:10 +0200

    first shot for calculator

--- basics/calculator/main.py
+++ basics/calculator/main.py
@@ -1,8 +1,32 @@
-numbers = input("Give me n numbers: ").split()
-
-for num in numbers:
-    print("The number is:", num)
 
 
-for i, v in enumerate(numbers):
-    print("The number #",i+1, "is:", v)
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a*b;
+
+def divide (a, b):
+    try:
+        return a/b;
+    except ZeroDivisionError:
+        print("You cannot divide by zero")
+
+
+final = 0
+while True:
+    print(final)
+    number = input("Enter a number: ")
+    operator = input("Enter a operator(+,-,x,*,/): ")
+    match operator:
+        case "+":
+          final = add(final, int(number))
+        case "-":
+            final = subtract(final, int(number))
+        case "X" | "*":
+            final = multiply(final, int(number))
+        case '/':
+            final = divide(final, int(number))
