61c0147a8d31226bea831a8809e073c8e148eb1e
Zakaria Ghoula <zghoula@worldia.com>
2026-05-28 13:50:50 +0200

    ask folder to organize and create a map of type/extentions

--- basics/ci-file-organizer/file-organizer.py
+++ basics/ci-file-organizer/file-organizer.py
@@ -2,33 +2,44 @@
 import os
 import shutil
 
-path = "./files"
-# create directories
-directories = ["images", "documents", "videos", "compressed", "app"]
-for directory in directories:
-    dir = path + '/' + directory
-    if not os.path.exists(dir):
-        os.makedirs(dir)
 
-contents = os.listdir(path)
-print("Directory contents:", contents)
+# ask folder to organize
+target_folder = input("Enter the name of folder to organize: ")
+
+# Define mapping of file extensions to folder names
+file_types = {
+    "images":['.jpg','.jpeg', '.png','.gif'],
+    "documents":['.pdf', '.txt', '.doc', '.xls', '.docs', '.xlsx'],
+    "videos":['.mp4', '.avi', '.mkv'],
+    "compressed":['.zip', '.rar', '.7z'],
+    "app":['.exe', '.msi', '.dmg']
+}
+
+# create directories if not exist
+for folder in file_types.keys():
+    folder_path = os.path.join(target_folder, folder)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
 
 # split files by extension and move to folder
-for file in contents:
-    source = path + "/" + file
-    root, ext = os.path.splitext(path + "/" + file)
-    if(ext ==''):
+for filename in os.listdir(target_folder):
+    file_path = os.path.join(target_folder, filename)
+    # Skip if it's a folder
+    if os.path.isdir(file_path):
         continue
-    match ext:
-        case ".txt" | '.pdf':
-            shutil.move(source, path + "/documents")
-        case ".jpg" | ".jpeg" | ".png":
-            shutil.move(source, path + "/images")
-        case ".mp4" | ".avi" | ".mkv":
-            shutil.move(source, path + "/videos")
-        case ".zip" | ".rar" | ".7z":
-            shutil.move(source, path + "/compressed")
-        case ".exe" | ".msi" | ".dmg":
-            shutil.move(source, path + "/app")
-        case _:
-            print(f"Unknown extension: {ext}")
+
+    #get extension of dile
+    root, ext = os.path.splitext(filename)
+
+    moved = False
+
+    for folder, extensions in file_types.items():
+        if ext.lower() in extensions:
+            dest_path = os.path.join(target_folder, folder, filename)
+            #move files
+            shutil.move(file_path, dest_path)
+            moved = True
+            break
+
+    if not moved:
+        print(f"⚠️ Skipped: {filename} (Unknown file type)")
